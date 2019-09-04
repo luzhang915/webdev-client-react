b682cb0393f7a35d4123052e48d25bc7c31c8952
Lu Zhang <luz20@pitt.edu>
2019-09-04 18:51:57 -0400

    first commit

---
+++ src/TopicPills.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class TopicPills extends React.Component {
+    render() {
+        return (
+            <ul className="nav nav-pills">
+                <li className="nav-item">
+                    <a className="nav-link active" href="#">Active</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        )
+    }
+}
+
+export default TopicPills;
